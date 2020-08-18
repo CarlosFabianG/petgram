eb0930ce8c476254ce9bc485d218b5cdf28795e1
Carlos Fabián <carl.fab.g@gmail.com>
2020-08-18 15:28:32 -0500

    creacion de custom hook useNearScreen

--- src/Components/PhotoCard/index.js
+++ src/Components/PhotoCard/index.js
@@ -1,30 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import { useLocalStorage } from '../../Hooks/useLocalStorage'
+import { useNearScreen } from '../../Hooks/useNearScreen'
 import { Article, WrapperImg, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, photocard, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  useEffect(() => {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver((entries) => {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
