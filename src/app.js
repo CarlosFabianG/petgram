79d919170b019bc0ccfd379034eb27c874e07afb
Carlos Fabián <carl.fab.g@gmail.com>
2020-08-20 15:57:53 -0500

    rutas protegidas

--- src/app.js
+++ src/app.js
@@ -4,9 +4,17 @@ import { GlobalStyle } from './styles/GlobalStyles'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
 import { NavBar } from './Components/NavBar'
 
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export function App () {
   return (
@@ -18,6 +26,21 @@ export function App () {
         <Home path='/pet/:id' />
         <Detail path='detail/:id' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              :
+              <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
