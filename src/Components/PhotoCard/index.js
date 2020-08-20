886caf1482a1a7558cd370ee53794f39742a49da
Carlos Fabián <carl.fab.g@gmail.com>
2020-08-19 21:07:40 -0500

    commit antes de pasar a instalar reach router

--- src/Components/PhotoCard/index.js
+++ src/Components/PhotoCard/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { useLocalStorage } from '../../Hooks/useLocalStorage'
 import { useNearScreen } from '../../Hooks/useNearScreen'
 import { Article, WrapperImg, Img } from './styles'
+
 import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  const handleFavClick = () => setLiked(!liked)
 
   return (
     <Article ref={element}>
@@ -21,7 +21,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </WrapperImg>
           </a>
-          <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+          <ToggleLikeMutation>
+            {
+              (toggleLike) => {
+                const handleFavClick = () => {
+                  !liked && toggleLike({
+                    variables: {
+                      input: { id }
+                    }
+                  })
+                  setLiked(!liked)
+                }
+                return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+              }
+            }
+          </ToggleLikeMutation>
         </>
       )}
     </Article>
